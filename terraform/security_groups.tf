82ad1ee3c8e11051ab156aa3d3ccb7cdb32e53ba
Rob Hough <rob.hough@gmail.com>
2019-01-23 13:36:37 -0500

    tf fmt

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,24 +1,24 @@
 resource "aws_security_group" "allow_all_vpc" {
-    name = "allow_all_vpc"
-    description = "Allows all traffic from this VPC to this VPC"
-    vpc_id = "${module.vpc.vpc_id}"
+  name        = "allow_all_vpc"
+  description = "Allows all traffic from this VPC to this VPC"
+  vpc_id      = "${module.vpc.vpc_id}"
 
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["192.168.10.0/24"]
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.10.0/24"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags {
-        Name = "allow_all_vpc"
-        Terraform = true
-    }
+  tags {
+    Name      = "allow_all_vpc"
+    Terraform = true
+  }
 }
