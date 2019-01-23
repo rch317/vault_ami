238053dea1fd3c0d4aaf18df4caf26eaf3e84b55
Rob Hough <rob.hough@gmail.com>
2019-01-23 10:02:24 -0500

    Moved providers to separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,2 @@
-provider "aws" {
-  version                 = "~> 1.54"
-  region                  = "${var.aws_region}"
-  shared_credentials_file = "${var.aws_shared_credentials_file}"
-  profile                 = "${var.aws_profile}"
+// main.tf
 
-  assume_role {
-    role_arn = "${var.aws_role_arn}"
-  }
-}
-
-provider "template" {
-  version = "~> 1.0"
-}
