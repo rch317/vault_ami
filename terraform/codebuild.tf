cb05681d130b485197421d94498dac5509f7e0f4
Rob Hough <rob.hough@gmail.com>
2019-02-14 10:02:17 -0500

    Terraform fmt

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -14,16 +14,19 @@ resource "aws_codebuild_project" "ami" {
     location        = "https://github.com/rch317/vault_ami"
     git_clone_depth = 1
   }
+
   vpc_config {
     vpc_id             = "${module.vpc.vpc_id}"
     subnets            = ["${module.vpc.private_subnets}"]
     security_group_ids = ["${aws_security_group.allow_all_vpc.id}"]
   }
+
   environment {
     compute_type = "BUILD_GENERAL1_MEDIUM"
+
     // image        = "aws/codebuild/ubuntu-base:14.04"
-    image        = "aws/codebuild/eb-python-2.6-amazonlinux-64:2.1.6"
-    type         = "LINUX_CONTAINER"
+    image = "aws/codebuild/eb-python-2.6-amazonlinux-64:2.1.6"
+    type  = "LINUX_CONTAINER"
 
     // privileged_mode = "true"
 
@@ -31,7 +34,6 @@ resource "aws_codebuild_project" "ami" {
       "name"  = "AWS_REGION"
       "value" = "${var.aws_region}"
     }
-
     environment_variable {
       "name"  = "AWS_VPC_ID"
       "value" = "${module.vpc.vpc_id}"
@@ -41,6 +43,7 @@ resource "aws_codebuild_project" "ami" {
       "value" = "${module.vpc.private_subnets[0]}"
     }
   }
+
   tags = {
     "Name"      = "vault_ami"
     "Terraform" = "true"
